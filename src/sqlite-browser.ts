3f4103c9235ba49b4f19e7bfd2a7a54944991f08
rpanadero <rpanadero@okode.com>
2018-01-10 09:44:49 +0100

    Implementing sqlbatch

--- src/sqlite-browser.ts
+++ src/sqlite-browser.ts
@@ -26,10 +26,48 @@ class SQLiteObjectBrowser extends SQLiteObject {
     });
   }
 
-  sqlBatch(sqlStatements: Array<string | string[] | any>): Promise<any> {
-    return new Promise((resolve, reject) => { reject('sqlBatch mock not implemented'); });
+  /**
+   * Do multiple sql operations.
+   * 
+   * @param sqlStatements statements which will be processed and executed
+   * Example:
+   * [
+   *   "INSERT...",
+   *   ["UPDATE ... (?, ?)", [1, 2]], // The query is on the first element and the params in he next
+   * ]
+   */
+  async sqlBatch(sqlStatements: Array<string | string[] | any>): Promise<any> {
+    if (sqlStatements == null || sqlStatements.length == 0) {
+      return Promise.reject('No statements');
+    }
+    for (let i = 0; i < sqlStatements.length; i++) {
+      let sqlStatement = sqlStatements[i];
+      let params: any[] = [];
+      let query = null;
+      if (sqlStatement instanceof String) {
+        query = sqlStatement as string;
+      } else if (sqlStatement instanceof Array) {
+        /**
+         * Trying to process an SQLStatement like ["UPDATE ... (?, ?)", [1, 2]]
+         */
+        let queryPos = 0, paramsPos = 1;
+        let querys = sqlStatement as Array<any>;
+        if (querys.length > 0 && querys[queryPos] instanceof String) {
+          // Looking for the query
+          query = querys[queryPos] as string;
+        }
+        if (query != null && querys.length > 1 && querys[paramsPos] instanceof Array) {
+          // Looking for the params
+          params = querys[paramsPos] as Array<any>;
+        }
+      }
+      if (query == null) {
+        return Promise.reject('SqlBatch contains an invalid query');
+      }
+      await this.executeSql(query, params);
+    }
+    return Promise.resolve('Success');
   }
-
 }
 
 export class SQLiteBrowser extends SQLite {
